a8a51ce540ffdb36865ad73f4362cd21bd71fbdb
Jae Doherty <jaeld97@gmail.com>
2020-12-01 15:33:30 -0800

    finished w11d2 proj

---
+++ pokedex_pt1/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,11 @@
+class Api::PokemonController < ApplicationController
+    def index
+        @pokemons = Pokemon.all
+        render json: @pokemons
+    end
+
+    def show
+        @pokemon = Pokemon.find_by(id: params[:id])
+        render json: @pokemon
+    end
+end
