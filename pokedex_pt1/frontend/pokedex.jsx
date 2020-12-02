1d667dd80f2ecfd438a2b8d3dba6f748658ffef7
Jae Doherty <jaeld97@gmail.com>
2020-12-02 14:36:17 -0800

    created store

--- pokedex_pt1/frontend/pokedex.jsx
+++ pokedex_pt1/frontend/pokedex.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions'
 import {fetchAllPokemon} from './util/api_util';
-import {receiveAllPokemon} from './actions/pokemon_actions';
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const rootEl = document.getElementById('root')
+    const store = configureStore();
 
+    window.requestAllPokemon = requestAllPokemon;
+    window.receiveAllPokemon = receiveAllPokemon;
     window.fetchAllPokemon = fetchAllPokemon;
-    window.receiveAllPokemon = receiveAllPokemon; 
+    window.getState = store.getState;
+    window.dispatch = store.dispatch; 
+
     
     ReactDOM.render(<h1>React is working</h1>, rootEl)
 })
